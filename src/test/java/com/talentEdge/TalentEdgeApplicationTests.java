33bce1f036dcaf03eebbc5f167d3a0dca44beb46
Avishka Chamod <avishkachamod14@gmail.com>
2025-10-13 10:01:36 +0530

    prject setup

---
+++ src/test/java/com/talentEdge/TalentEdgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentEdge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentEdgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
