facce86c7ad477fa36a6895e0d4ea9cbee629611
Avishka Chamod <avishkachamod14@gmail.com>
2025-10-20 11:36:27 +0530

    api services created for company registration

--- src/main/java/com/talentEdge/controller/CompanyController.java
+++ src/main/java/com/talentEdge/controller/CompanyController.java
@@ -2,6 +2,8 @@ package com.talentEdge.controller;
 
 import com.talentEdge.dto.LogInRequest;
 import com.talentEdge.dto.LogInResponse;
+import com.talentEdge.dto.RegistrationResponse;
+import com.talentEdge.model.CompanyEntity;
 import com.talentEdge.service.CompanyServices;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,11 @@ public class CompanyController {
     @GetMapping("/login")
     public ResponseEntity<LogInResponse> companyLogIn(@RequestBody LogInRequest request , HttpServletResponse response){
         return ResponseEntity.ok(companyServices.companyLogIn(request , response));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegistrationResponse> companyRegister(@RequestBody CompanyEntity company , HttpServletResponse response){
+        return ResponseEntity.ok(companyServices.companyRegister(company , response));
     }
 
 }
--- src/main/java/com/talentEdge/service/CompanyServices.java
+++ src/main/java/com/talentEdge/service/CompanyServices.java
@@ -2,6 +2,7 @@ package com.talentEdge.service;
 
 import com.talentEdge.dto.LogInRequest;
 import com.talentEdge.dto.LogInResponse;
+import com.talentEdge.dto.RegistrationResponse;
 import com.talentEdge.model.CompanyEntity;
 import com.talentEdge.repo.CompanyRepository;
 import com.talentEdge.security.JwtUtil;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class CompanyServices {
@@ -51,5 +53,46 @@ public class CompanyServices {
         return new LogInResponse(token , true);
 
     }
+
+    public RegistrationResponse companyRegister(CompanyEntity company , HttpServletResponse response){
+
+        if(companyRepository.findByEmail(company.getEmail()).isPresent()){
+            return new RegistrationResponse("Company E-Mail Already used" , false);
+        }
+
+        if(company.getAbout().isEmpty()){
+            return new RegistrationResponse("Please Enter the About" , false);
+        }else if(company.getContact().isEmpty()){
+            return new RegistrationResponse("Please Enter the Contact" , false);
+        }else if(company.getWebUrl().isEmpty()){
+            return new RegistrationResponse("Please Enter the Web Url" , false);
+        }else{
+
+            Random random = new Random();
+            int number;
+            do{
+                number = 100+ random.nextInt(900);
+            }while(companyRepository.existsById(number));
+
+            company.setId(number);
+            company.setAbout(company.getAbout());
+            company.setEmail(company.getEmail());
+            company.setContact(company.getContact());
+            company.setWebUrl(company.getWebUrl());
+            company.setPassword(passwordEncoder.encode(company.getPassword()));
+
+            companyRepository.save(company);
+
+            ResponseCookie cookie = ResponseCookie.from("company" , String.valueOf(number))
+                    .httpOnly(false)
+                    .path("/")
+                    .maxAge(24 * 60 * 60)
+                    .build();
+            response.addHeader("Set-Cookie" , cookie.toString());
+
+            return new RegistrationResponse("Company Successfully Saved" , true);
+        }
+    }
+
 
 }
