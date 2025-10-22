02ae57596e7b680c4feb933c979d6bf2dc5c3f3c
Avishka Chamod <avishkachamod14@gmail.com>
2025-10-22 18:00:12 +0530

    api services created for fetch job data

--- src/main/java/com/talentEdge/controller/JobPostsController.java
+++ src/main/java/com/talentEdge/controller/JobPostsController.java
@@ -1,10 +1,13 @@
 package com.talentEdge.controller;
 
+import com.talentEdge.dto.JobPostsDTO;
 import com.talentEdge.dto.LogInResponse;
 import com.talentEdge.model.JobPosts;
 import com.talentEdge.service.JobPostsServices;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/jobposts")
@@ -19,10 +22,14 @@ public class JobPostsController {
 
     @PostMapping("/open")
     public ResponseEntity<LogInResponse> openNewJobPost(@RequestBody JobPosts jobPosts){
-
         LogInResponse response = jobPostsServices.openNewJoPost(jobPosts);
         return ResponseEntity.ok(response);
-
     }
+
+    @GetMapping("/fetch/{companyId}")
+    public List<JobPostsDTO> getJobsByCompany(@PathVariable Integer companyId) {
+        return jobPostsServices.fetchJobOpeningsByID(companyId);
+    }
+
 
 }
